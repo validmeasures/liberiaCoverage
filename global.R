62f5a3aabfb014afb33b419d4fca5a577dc2c71a
Ernest Guevarra <ernest@validmeasures.org>
2018-08-25 13:47:09 +0000

    update global.R

--- global.R
+++ global.R
@@ -23,10 +23,11 @@ if(!require(shiny)) install.packages("shiny")
 if(!require(shinydashboard)) install.packages("shinydashboard")
 if(!require(devtools)) install.packages("devtools")
 if(!require(bbw)) install.packages("bbw")
+if(!require(shinyjs)) install.packages("shinyjs")
 if(!require(liberia)) install_github("liberia")
 if(!require(spatialsampler)) install_github("spatialsampler")
 if(!require(odkr)) install_github("odkr")
 if(!require(zscorer)) install_github("zscorer")
-if(!require(sampsizer)) install_github("sampsizer")
+if(!require(sampsizer)) install_github("validmeasures/sampsizer")
 
 
